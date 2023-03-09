f2dd65f6ee45f50f99da487222d79e398671f74c
jakub.vlasak <jakub.vlasak@doosan.com>
2023-03-09 14:47:01 +0100

    Prvni komit

---
+++ testik_gitik.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+main_window = Tk()
+main_window.title('Kube si zkousi Gitik')
+main_window.geometry('320x240')
+main_window.iconbitmap(main_window, 'znak_skoda.ico')
+main_window.mainloop()
+
+
