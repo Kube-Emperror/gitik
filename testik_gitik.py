96ba01455f8e36ca7e927a699352eef2f898db62
jakub.vlasak <jakub.vlasak@doosan.com>
2023-03-10 10:23:40 +0100

    Přidané prvky

--- testik_gitik.py
+++ testik_gitik.py
@@ -2,8 +2,10 @@ from tkinter import *
 
 main_window = Tk()
 main_window.title('Kube si zkousi Gitik')
-main_window.geometry('320x240')
+main_window.geometry('320x320')
 main_window.iconbitmap(main_window, 'znak_skoda.ico')
+canvas = Canvas(main_window, width=320, height=240)
+canvas.pack()
 main_window.mainloop()
 
 
